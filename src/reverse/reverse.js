a5f5f116e8ac70dc1847d4ecb6e70bb95df676b4
ShaunDM <123577673+ShaunDM@users.noreply.github.com>
2024-01-04 14:16:45 -0500

    update
    Update after completing lesson related to forked repo

--- src/reverse/reverse.js
+++ src/reverse/reverse.js
@@ -9,17 +9,25 @@ const LinkedList = require("../lib/linkedList");
  * @returns {LinkedList}
  * A linked list in reverse order
  */
-function reverseIterative(list) {}
-
-/**
- * Recursively reverse a linked list
- *
- * @param list
- * A (potentially empty) linked list
- *
- * @returns {LinkedList}
- * A linked list in reverse order
- */
-function reverseRecursive(list) {}
+function reverseIterative(list) {
+    const reversedList = new LinkedList();
+    let node = list.head;
+    while (node) {
+      reversedList.insertAtHead(node.value);
+      node = node.next;
+    }
+    return reversedList;
+}
+function reverseRecursive(list) {
+    if (list.length <= 1) {
+      return list;
+    }
+  
+    const head = list.head.value;
+    list.remove((node, index) => index === 0);
+    const reversedList = reverseRecursive(list);
+    reversedList.insert(head);
+    return reversedList;
+  }
 
 module.exports = { reverseIterative, reverseRecursive };
--- src/swap/swap.js
+++ src/swap/swap.js
@@ -4,6 +4,39 @@
  * @param {Node} x
  * @param {Node} y
  */
-function swap(list, x, y) {}
+function findPrevious(list, node){
+    let search = list.head;
+    let previous = null;
+    while(search){
+        if(search === node){
+            return previous;
+        }
+        previous = search;
+        search = search.next;
+    }
+    return null;
+}
+
+function swap(list, x, y) {
+    if(!list.head) return list;
+    const xNext = x.next;
+    const x_prev = list.findWithPrevious((node) => node === x)[1];
+    const y_prev = list.findWithPrevious((node) => node === y)[1];
+
+    x.next = y.next;
+    y.next = xNext;
+
+    if(x_prev){
+        x_prev.next = y;
+    }else {
+       list.head = y;
+    }
+    if(y_prev){
+        y_prev.next = x;
+    }else {
+       list.head = x;
+    }
+    return list;
+}
 
 module.exports = swap;
